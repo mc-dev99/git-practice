72942abf2b96be30faa9de94239256ac8d222b44
Irene <choireneh@gmail.com>
2022-09-23 17:23:33 -0400

    merged AGAIN

--- function_d.py
+++ function_d.py
@@ -1,11 +1,4 @@
 def max_value(numbers):
-<<<<<<< HEAD
-
-=======
-    """ This function returns the largest number
-        in the list.
-    """
->>>>>>> 7a854e81e12152fa063d15c1b0e996baba3e760b
     return max(numbers)
 
 
